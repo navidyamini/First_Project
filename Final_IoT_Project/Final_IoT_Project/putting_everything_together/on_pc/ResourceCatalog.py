e42b13bbb19d5794cd96912b0dbd533ef98ab842
navidyamini <navidyamini@gmail.com>
2017-12-09 22:42:23 +0100

    finishg the resorce catolog

---
+++ Final_IoT_Project/Final_IoT_Project/putting_everything_together/on_pc/ResourceCatalog.py
@@ -0,0 +1,27 @@
+import cherrypy
+
+class ResourceCatalog(object):
+
+    exposed = True
+
+    def GET(self, *uri, **params):
+
+        try:
+            file = open("../RawWebpage/initial_data.json", "r")
+            json_string = file.read()
+            file.close()
+        except:
+            raise KeyError("***** ERROR IN READING JSON FILE RELATED TO RESOURCES *****")
+        return json_string
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True,
+        }
+    }
+    # cherrypy.quickstart(WebCalculator(),'/',conf)
+    cherrypy.tree.mount(ResourceCatalog(), '/', conf)
+    cherrypy.server.socket_host = '192.168.1.65'
+    cherrypy.engine.start()
+    cherrypy.engine.block()
--- Final_IoT_Project/Final_IoT_Project/putting_everything_together/on_pc/TelegramBotSetting.py
+++ Final_IoT_Project/Final_IoT_Project/putting_everything_together/on_pc/TelegramBotSetting.py
@@ -7,7 +7,7 @@ class BrokerSetting(object):
 
     def GET(self, *uri):
         try:
-            if (uri[0] == "broker"):
+            if (uri[0] == "telegram"):
                 return '''<!DOCTYPE html>
                         <html>
                             <head>
@@ -25,23 +25,14 @@ class BrokerSetting(object):
                             </head>
                         <body>
 
-                        <h1>Museum broker connection settings</h1>
-                        <p>Please enter the broker connection info here.</p>
+                        <h1>Museum telegram connection settings</h1>
+                        <p>Please enter the telegram connection info here.</p>
 
                         <form action="/reply" method="POST">
                             <table>
                                 <fieldset>
-                                    Broker_IP:<br>
-                                    <input type="text" name="Broker_IP1">
-                                    <br/>
-                                    Broker_port:<br>
-                                    <input type="text" name="Broker_port1">
-                                    <br/>
-                                    DHT_Topic:<br>
-                                    <input type="text" name="DHT_Topic1">
-                                    <br/>
-                                    AC_Topic:<br>
-                                    <input type="text" name="AC_Topic1">
+                                    Port:<br>
+                                    <input type="text" name="Port1">
                                     <br/>
                                 </fieldset>
                                 <button> Submit </button>
@@ -53,7 +44,7 @@ class BrokerSetting(object):
                         </html>
                         '''
         except:
-            raise KeyError("***** ERROR IN OPENING THE PAGE /broker PLEASE WRITE host:port/broker IN URL*****")
+            raise KeyError("***** ERROR IN OPENING THE PAGE /broker PLEASE WRITE host:port/telegram IN URL*****")
         else:
             return cherrypy.request.body.read()
 
@@ -63,11 +54,8 @@ class BrokerSetting(object):
             with open("../RawWebpage/initial_data.json", 'r') as json_data_file:
                 obj = json.load(json_data_file)
 
-            dict_data = obj['broker']
-            obj['broker'] = {"Broker_IP": str(params['Broker_IP1']),
-                             "Broker_port": str(params['Broker_port1']),
-                             "DHT_Topic": str(params['DHT_Topic1']),
-                             "AC_Topic": str(params['AC_Topic1'])}
+            dict_data = obj['telegram']
+            obj['telegram'] = {"Port": str(params['Port1'])}
         except:
             raise KeyError("***** ERROR IN READING THE JSON FILE*****")
         try:
@@ -92,23 +80,14 @@ class BrokerSetting(object):
                         </head>
                     <body>
 
-                    <h1>Museum broker connection settings</h1>
-                    <p>Successfully writing the broker connection info into the json file.</p>
+                    <h1>Museum telegram connection settings</h1>
+                    <p>Successfully writing the telegram connection info into the json file.</p>
 
                     <form action="/reply" method="POST">
                         <table>
                             <fieldset>
-                                Broker_IP:<br>
-                                <input type="text" name="Broker_IP1">
-                                <br/>
-                                Broker_port:<br>
-                                <input type="text" name="Broker_port1">
-                                <br/>
-                                DHT_Topic:<br>
-                                <input type="text" name="DHT_Topic1">
-                                <br/>
-                                AC_Topic:<br>
-                                <input type="text" name="AC_Topic1">
+                                Port:<br>
+                                <input type="text" name="Port1">
                                 <br/>
                             </fieldset>
                             <button> Submit </button>
