1b4e2e970b6bda6fb0e88f17c550312b8aa9be1c
Ximeroga <ximenagarzon22@gmail.com>
2018-07-02 16:01:35 +0200

    UPDATE VALUES POST

--- Final_IoT_Project/Final_IoT_Project/putting_everything_together/on_pc/POST_ResourceCatalog.py
+++ Final_IoT_Project/Final_IoT_Project/putting_everything_together/on_pc/POST_ResourceCatalog.py
@@ -1,5 +1,6 @@
 import cherrypy
 import json
+
 class ResourceCatalog(object):
 
     exposed = True
@@ -16,23 +17,52 @@ class ResourceCatalog(object):
 
     def POST(self, *uri, **params):
         try:
-            file = open("results.json","r")
-            json_string = file.read()
-            file.close()
-            data = json.loads(json_string)
-            if (uri[0]=="thingSpeak"):
+            with open("../RawWebpage/initial_data.json", "r") as idata:
+                inidata = json.loads(idata.read())
+                data = cherrypy.request.body.read()
+                newdata = json.loads(data)
 
-                data['thingspeak'] = {"THINGSPEAK_HOST": str(uri[1]),
-                             "mqttHost": str(uri[2]),
-                             "tPort": str(uri[3]),
-                             "channelID": str(uri[4]),
-                             "tTransport": str(uri[5]),
-                             "ACCESS_TOKEN": str(uri[6]),
-                             "READ_API_KEY": str(uri[7])}
-                file2 = open("results.json", "w")
-                json.dump(data, file2)
-                return "Done!"
-        except:
+                print data
+                print inidata
+                print newdata
+                key = list(newdata.keys())[0]
+
+
+
+                if key =='thresholds':
+
+                    inidata['thresholds']['min_hum'] = newdata['thresholds']['min_hum']
+                    inidata['thresholds']['min_temp'] = newdata['thresholds']['min_temp']
+                    inidata['thresholds']['max_temp'] = newdata['thresholds']['max_temp']
+                    inidata['thresholds']['max_hum'] = newdata['thresholds']['max_hum']
+                elif key=='thingspeak':
+                    inidata['thingspeak']['READ_API_KEY'] = newdata['thingspeak']['READ_API_KEY']
+                    inidata['thingspeak']['ACCESS_TOKEN'] = newdata['thingspeak']['ACCESS_TOKEN']
+                    inidata['thingspeak']['tTransport'] = newdata['thingspeak']['tTransport']
+                    inidata['thingspeak']['channelID'] = newdata['thingspeak']['channelID']
+                    inidata['thingspeak']['tPort'] = newdata['thingspeak']['tPort']
+                    inidata['thingspeak']['mqttHost'] = newdata['thingspeak']['mqttHost']
+                    inidata['thingspeak']['THINGSPEAK_HOST'] = newdata['thingspeak']['THINGSPEAK_HOST']
+                elif key == 'broker':
+                    inidata['broker']['Broker_IP'] = newdata['broker']['Broker_IP']
+                    inidata['broker']['DHT_Topic'] = newdata['broker']['DHT_Topic']
+                    inidata['broker']['AC_Topic'] = newdata['broker']['AC_Topic']
+                    inidata['broker']['Ac_Status'] = newdata['broker']['Ac_Status']
+                    inidata['broker']['Broker_port'] = newdata['broker']['Broker_port']
+                    inidata['broker']['Counter_Topic'] = newdata['broker']['Counter_Topic']
+                elif key == 'telegram':
+                    inidata['telegram']['Port'] = newdata['telegram']['Port']
+                    inidata['telegram']['chatID'] = newdata['telegram']['chatID']
+                elif key == 'dataToRest':
+                    inidata['dataToRest']['Host_IP'] = newdata['dataToRest']['Host_IP']
+                    inidata['dataToRest']['port'] = newdata['dataToRest']['port']
+
+            with open("../RawWebpage/initial_data.json", "w") as file:
+                json.dump(inidata, file)
+                return "UPDATED"
+
+        except  Exception as e:
+            print ("error:",e)
             return "Problem in updating file"
 
 if __name__ == '__main__':
