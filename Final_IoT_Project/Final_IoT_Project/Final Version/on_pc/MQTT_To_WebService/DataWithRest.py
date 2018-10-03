b1f3f6ae2665f1d6a050a1daaf0e0530c446350b
navidyamini <navidyamini@gmail.com>
2018-10-03 16:51:47 +0200

    writing comments

--- Final_IoT_Project/Final_IoT_Project/Final Version/on_pc/MQTT_To_WebService/DataWithRest.py
+++ Final_IoT_Project/Final_IoT_Project/Final Version/on_pc/MQTT_To_WebService/DataWithRest.py
@@ -1,12 +1,13 @@
 import cherrypy
 import json
 import requests
-
+# this web service read the real time data and expose it on the given url
 
 class DataWithRest(object):
     exposed = True
 
     def GET(self, *uri, **params):
+# reading the real time data file when it recives the request
 
         try:
             file = open("real_time_data.json", "r")
@@ -16,6 +17,7 @@ class DataWithRest(object):
         except:
             raise KeyError("***** DataWithRest: ERROR IN READING JSON FILE RELATED TO DATA *****")
         results = json.loads(json_string)
+       # if the item exist in side the real time data json file it will return the asked data
         if (item == "all"):
             return json_string
 
@@ -35,24 +37,25 @@ class DataWithRest(object):
             elif (uri[1] == 'all'):
                 return str(json.dumps(results[item]))
         else:
+            # in case of wrong input it will return this message
             return "Nothing found, check the input again"
 
 
 if __name__ == '__main__':
-
+# reading the reaource catalog url from the json file
     try:
-        file = open("config_file.json", "r")  # Navid's Local IP address
-        # file = open("config_file_xime.json", "r") #Ximena's local IP address
+        file = open("config_file.json", "r")
         json_string = file.read()
         file.close()
     except:
         raise KeyError("***** DataWithRest: ERROR IN READING CONFIG FILE *****")
-
+# sending request to the resource catalog to ask for the MQTT to web service URL
     config_json = json.loads(json_string)
     url = config_json["reSourceCatalog"]["url"]
 
     respond = requests.get(url + "/dataToRest")
     json_format = json.loads(respond.text)
+# set the ip and the port that the mqtt to web service should expose the data on it
     Host_IP = json_format["Host_IP"]
     port = json_format["port"]
 
